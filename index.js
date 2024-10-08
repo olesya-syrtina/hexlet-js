0c0953480dc1559183c3088b53f7851fe7382d99
Олеся Сыртина <olesademcenko87091@gmail.com>
2024-10-08 22:59:19 +0300

    Изменение точки входа проекта

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import _ from "lodash";
+import main from './src/index.js';
 
-console.log(_.last(["one", "two"]));
+main();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from "lodash";
+
+const main = () => {
+    console.log(_.last(["one", "two"]));
+}
+
+export default main;
